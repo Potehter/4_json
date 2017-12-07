7622c3c8d224511daaeb023905999e53389a2091
Dmitrii <poteher@gmail.com>
2017-12-07 22:32:45 +0300

    edited names of vars in load_data

--- pprint_json.py
+++ pprint_json.py
@@ -1,18 +1,19 @@
-import json, sys
+import json
+import sys
 
 
 def load_data(filepath):
-	with open(filepath, 'r') as file_to_convert:
-		file_content_json = json.loads(file_to_convert.read())
-	return file_content_json
+    with open(filepath, 'r') as source_file:
+        file_content = json.loads(source_file.read())
+    return file_content
 
 def pretty_print_json(text_json):
-	print(json.dumps(text_json, sort_keys=True, indent=4, ensure_ascii=False))
+    print(json.dumps(text_json, sort_keys=True, indent=4, ensure_ascii=False))
 
 
 def main():
-	text_json = load_data(sys.argv[1])
-	pretty_print_json(text_json)
+    text_json = load_data(sys.argv[1])
+    pretty_print_json(text_json)
 
 if __name__ == '__main__':
     main()
